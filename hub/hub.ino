d8c529ad7487965c45def38df4b318c5b85ff7b7
-T.K.- <t_k_233@outlook.com>
2022-06-23 22:29:03 -0700

    ADD: upgrade to NLSM format packet, trim down unnecessary code

--- hub/hub.ino
+++ hub/hub.ino
@@ -12,129 +12,46 @@
  any redistribution
 *********************************************************************/
 
-/* This sketch demonstrates the central API() that allows you to connect
- * to multiple peripherals boards (Bluefruit nRF52 in peripheral mode, or
- * any Bluefruit nRF51 boards).
- *
- * One or more Bluefruit boards, configured as a peripheral with the
- * bleuart service running are required for this demo.
- *
- * This sketch will: 
- *  - Read data from the HW serial port (normally USB serial, accessible
- *    via the Serial Monitor for example), and send any incoming data to
- *    all other peripherals connected to the central device.
- *  - Forward any incoming bleuart messages from a peripheral to all of
- *    the other connected devices.
- * 
- * It is recommended to give each peripheral board a distinct name in order
- * to more easily distinguish the individual devices.
- * 
+/* 
  * Connection Handle Explanation
  * -----------------------------
  * The total number of connections is BLE_MAX_CONNECTION (20)
  * 
- * The 'connection handle' is an integer number assigned by the SoftDevice
- * (Nordic's proprietary BLE stack). Each connection will receive it's own
- * numeric 'handle' starting from 0 to BLE_MAX_CONNECTION-1, depending on the order
- * of connection(s).
- *
- * - E.g If our Central board connects to a mobile phone first (running as a peripheral),
- * then afterwards connects to another Bluefruit board running in peripheral mode, then
- * the connection handle of mobile phone is 0, and the handle for the Bluefruit
- * board is 1, and so on.
  */
 
-/* LED PATTERNS
- * ------------
- * LED_RED   - Blinks pattern changes based on the number of connections.
- * LED_BLUE  - Blinks constantly when scanning
- */
 
 #include <bluefruit.h>
 
-// Struct containing peripheral info
-typedef struct
-{
+typedef struct {
   char name[16+1];
-
   uint16_t conn_handle;
-
-  // Each prph need its own bleuart client service
-  BLEClientUart bleuart;
+  BLEClientBas client_battery_info;
+  BLEClientDis client_device_info;
+  BLEClientUart client_uart;
 } prph_info_t;
 
-/* Peripheral info array (one per peripheral device)
- * 
- * There are 'BLE_MAX_CONNECTION' central connections, but the
- * the connection handle can be numerically larger (for example if
- * the peripheral role is also used, such as connecting to a mobile
- * device). As such, we need to convert connection handles <-> the array
- * index where appropriate to prevent out of array accesses.
- * 
- * Note: One can simply declares the array with BLE_MAX_CONNECTION and use connection
- * handle as index directly with the expense of SRAM.
- */
 prph_info_t prphs[BLE_MAX_CONNECTION];
 
-// Software Timer for blinking the RED LED
-SoftwareTimer blinkTimer;
-uint8_t connection_num = 0; // for blink pattern
 
-void setup() 
-{
-  Serial.begin(115200);
-  while ( !Serial ) delay(10);   // for nrf52840 with native usb
-
-  // Initialize blinkTimer for 100 ms and start it
-  blinkTimer.begin(100, blink_timer_callback);
-  blinkTimer.start();
-
-  Serial.println("Bluefruit52 Central Multi BLEUART Example");
-  Serial.println("-----------------------------------------\n");
-  
-  // Initialize Bluefruit with max concurrent connections as Peripheral = 0, Central = 4
-  // SRAM usage required by SoftDevice will increase with number of connections
-  Bluefruit.begin(0, 4);
-
-  // Set Name
-  Bluefruit.setName("Bluefruit52 Central");
-  
-  // Init peripheral pool
-  for (uint8_t idx=0; idx<BLE_MAX_CONNECTION; idx++)
-  {
-    // Invalid all connection handle
-    prphs[idx].conn_handle = BLE_CONN_HANDLE_INVALID;
-    
-    // All of BLE Central Uart Serivce
-    prphs[idx].bleuart.begin();
-    prphs[idx].bleuart.setRxCallback(bleuart_rx_callback);
+/**
+ * Find the connection handle in the peripheral array
+ * @param conn_handle Connection handle
+ * @return array index if found, otherwise -1
+ */
+int findConnHandle(uint16_t conn_handle) {
+  for(int id=0; id<BLE_MAX_CONNECTION; id+=1) {
+    if (conn_handle == prphs[id].conn_handle) {
+      return id;
+    }
   }
-
-  // Callbacks for Central
-  Bluefruit.Central.setConnectCallback(connect_callback);
-  Bluefruit.Central.setDisconnectCallback(disconnect_callback);
-
-  /* Start Central Scanning
-   * - Enable auto scan if disconnected
-   * - Interval = 100 ms, window = 80 ms
-   * - Filter only accept bleuart service in advertising
-   * - Don't use active scan (used to retrieve the optional scan response adv packet)
-   * - Start(0) = will scan forever since no timeout is given
-   */
-  Bluefruit.Scanner.setRxCallback(scan_callback);
-  Bluefruit.Scanner.restartOnDisconnect(true);
-  Bluefruit.Scanner.setInterval(160, 80);       // in units of 0.625 ms
-  Bluefruit.Scanner.filterUuid(BLEUART_UUID_SERVICE);
-  Bluefruit.Scanner.useActiveScan(false);       // Don't request scan response data
-  Bluefruit.Scanner.start(0);                   // 0 = Don't stop scanning after n seconds
+  return -1;  
 }
 
 /**
  * Callback invoked when scanner picks up an advertising packet
  * @param report Structural advertising data
  */
-void scan_callback(ble_gap_evt_adv_report_t* report)
-{
+void onScanHandler(ble_gap_evt_adv_report_t* report) {
   // Since we configure the scanner with filterUuid()
   // Scan callback only invoked for device with bleuart service advertised  
   // Connect to the device with bleuart service in advertising packet
@@ -145,43 +62,32 @@ void scan_callback(ble_gap_evt_adv_report_t* report)
  * Callback invoked when an connection is established
  * @param conn_handle
  */
-void connect_callback(uint16_t conn_handle)
-{
+void onConnectHandler(uint16_t conn_handle) {
   // Find an available ID to use
-  int id  = findConnHandle(BLE_CONN_HANDLE_INVALID);
+  int id = findConnHandle(BLE_CONN_HANDLE_INVALID);
 
   // Eeek: Exceeded the number of connections !!!
-  if ( id < 0 ) return;
+  if (id < 0) return;
   
   prph_info_t* peer = &prphs[id];
   peer->conn_handle = conn_handle;
   
   Bluefruit.Connection(conn_handle)->getPeerName(peer->name, sizeof(peer->name)-1);
 
-  Serial.print("Connected to ");
-  Serial.println(peer->name);
+  // battery information
+  if (peer->client_battery_info.discover(conn_handle)) {
+    int battery = peer->client_battery_info.read();
+  }
+  
+  if (peer->client_uart.discover(conn_handle)) {
+    peer->client_uart.setTimeout(100);
+    peer->client_uart.enableTXD();
 
-  Serial.print("Discovering BLE UART service ... ");
-
-  if ( peer->bleuart.discover(conn_handle) )
-  {
-    Serial.println("Found it");
-    Serial.println("Enabling TXD characteristic's CCCD notify bit");
-    peer->bleuart.enableTXD();
-
-    Serial.println("Continue scanning for more peripherals");
     Bluefruit.Scanner.start(0);
-
-    Serial.println("Enter some text in the Serial Monitor to send it to all connected peripherals:");
-  } else
-  {
-    Serial.println("Found ... NOTHING!");
-
-    // disconnect since we couldn't find bleuart service
+  }
+  else {
     Bluefruit.disconnect(conn_handle);
-  }  
-
-  connection_num++;
+  }
 }
 
 /**
@@ -189,129 +95,91 @@ void connect_callback(uint16_t conn_handle)
  * @param conn_handle
  * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
  */
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-  (void) conn_handle;
-  (void) reason;
-
-  connection_num--;
-
+void onDisconnectHandler(uint16_t conn_handle, uint8_t reason) {
   // Mark the ID as invalid
-  int id  = findConnHandle(conn_handle);
+  int id = findConnHandle(conn_handle);
 
-  // Non-existant connection, something went wrong, DBG !!!
-  if ( id < 0 ) return;
+  if (id < 0) return;
 
-  // Mark conn handle as invalid
   prphs[id].conn_handle = BLE_CONN_HANDLE_INVALID;
-
-  Serial.print(prphs[id].name);
-  Serial.println(" disconnected!");
 }
 
-/**
- * Callback invoked when BLE UART data is received
- * @param uart_svc Reference object to the service where the data 
- * arrived.
- */
-void bleuart_rx_callback(BLEClientUart& uart_svc)
-{
-  // uart_svc is prphs[conn_handle].bleuart
-  uint16_t conn_handle = uart_svc.connHandle();
+void BLE_init() {
+  Bluefruit.begin(0, 8);    // Peripheral = 0, Central = 4
 
-  int id = findConnHandle(conn_handle);
-  prph_info_t* peer = &prphs[id];
-  
-  // Print sender's name
-  Serial.printf("[From %s]: ", peer->name);
+  Bluefruit.setName("Hub");
 
-  // Read then forward to all peripherals
-  while ( uart_svc.available() )
-  {
-    // default MTU with an extra byte for string terminator
-    char buf[20+1] = { 0 };
+  for (uint8_t id=0; id<BLE_MAX_CONNECTION; id+=1) {
+    prphs[id].conn_handle = BLE_CONN_HANDLE_INVALID;
+
+    prphs[id].client_battery_info.begin();
+
+    prphs[id].client_device_info.begin();
     
-    if ( uart_svc.read(buf,sizeof(buf)-1) )
-    {
-      Serial.println(buf);
-      sendAll(buf);
-    }
+    prphs[id].client_uart.begin();
   }
+
+  Bluefruit.Central.setConnectCallback(onConnectHandler);
+  Bluefruit.Central.setDisconnectCallback(onDisconnectHandler);
+  
+  Bluefruit.Scanner.setRxCallback(onScanHandler);
+  Bluefruit.Scanner.restartOnDisconnect(true);
+  Bluefruit.Scanner.setInterval(160, 80);       // in units of 0.625 ms
+  Bluefruit.Scanner.filterUuid(BLEUART_UUID_SERVICE);
+  Bluefruit.Scanner.useActiveScan(false);       // Don't request scan response data
+  Bluefruit.Scanner.start(0);                   // 0 = Don't stop scanning after n seconds
 }
 
 /**
  * Helper function to send a string to all connected peripherals
  */
-void sendAll(const char* str)
-{
-  Serial.print("[Send to All]: ");
-  Serial.println(str);
-  
-  for(uint8_t id=0; id < BLE_MAX_CONNECTION; id++)
-  {
+void BLE_broadcast(const uint8_t* str, uint16_t size) {
+  for(uint8_t id=0; id < BLE_MAX_CONNECTION; id+=1) {
     prph_info_t* peer = &prphs[id];
 
-    if ( peer->bleuart.discovered() )
-    {
-      peer->bleuart.print(str);
+    if (peer->client_uart.discovered()) {
+      peer->client_uart.write(str, size);
     }
   }
 }
 
-void loop()
-{
-  // First check if we are connected to any peripherals
-  if ( Bluefruit.Central.connected() )
-  {
-    // default MTU with an extra byte for string terminator
-    char buf[20+1] = { 0 };
-    
-    // Read from HW Serial (normally USB Serial) and send to all peripherals
-    if ( Serial.readBytes(buf, sizeof(buf)-1) )
-    {
-      sendAll(buf);
-    }
-  }
+void setup() {
+  Serial.begin(115200);
+  
+  while (!Serial) {}
+  
+  Serial.setTimeout(100);
+
+  BLE_init();
 }
 
-/**
- * Find the connection handle in the peripheral array
- * @param conn_handle Connection handle
- * @return array index if found, otherwise -1
- */
-int findConnHandle(uint16_t conn_handle)
-{
-  for(int id=0; id<BLE_MAX_CONNECTION; id++)
-  {
-    if (conn_handle == prphs[id].conn_handle)
-    {
-      return id;
+void loop() {
+  if (Bluefruit.Central.connected()) {
+    if (Serial.available()) {
+      
+      uint8_t buffer[128] = {0};
+      uint16_t size = Serial.readBytesUntil('\n', buffer, 128);
+      BLE_broadcast(buffer, size);
+      
+      // we DO NOT need to send another newline character here.
+      // `Serial.readBytesUntil()` will store the ending newline char into the buf.
     }
   }
 
-  return -1;  
-}
+  
+  for (uint8_t id=0; id<BLE_MAX_CONNECTION; id+=1) {
+    if (prphs[id].conn_handle == BLE_CONN_HANDLE_INVALID) {
+      continue;
+    }
 
-/**
- * Software Timer callback is invoked via a built-in FreeRTOS thread with
- * minimal stack size. Therefore it should be as simple as possible. If
- * a periodically heavy task is needed, please use Scheduler.startLoop() to
- * create a dedicated task for it.
- * 
- * More information http://www.freertos.org/RTOS-software-timer.html
- */
-void blink_timer_callback(TimerHandle_t xTimerID)
-{
-  (void) xTimerID;
-
-  // Period of sequence is 10 times (1 second). 
-  // RED LED will toggle first 2*n times (on/off) and remain off for the rest of period
-  // Where n = number of connection
-  static uint8_t count = 0;
-
-  if ( count < 2*connection_num ) digitalToggle(LED_RED);
-  if ( count % 2 && digitalRead(LED_RED)) digitalWrite(LED_RED, LOW); // issue #98
-
-  count++;
-  if (count >= 10) count = 0;
+    if (prphs[id].client_uart.available()) {
+      uint8_t buffer[128];
+      uint16_t size = prphs[id].client_uart.readBytesUntil('\n', buffer, 128);
+      Serial.write(buffer, size);
+      
+      // we DO need to send another newline character here.
+      // `uart_svc.readBytesUntil()` will NOT store the ending newline char into the buf.
+      Serial.write('\n');
+    }
+  }
 }
--- node/node.ino
+++ node/node.ino
@@ -1,149 +1,132 @@
 /*********************************************************************
- This is an example for our nRF52 based Bluefruit LE modules
+  This is an example for our nRF52 based Bluefruit LE modules
 
- Pick one up today in the adafruit shop!
+  Pick one up today in the adafruit shop!
 
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
+  MIT license, check LICENSE for more information
+  All text above, and the splash screen below must be included in
+  any redistribution
 *********************************************************************/
 #include <bluefruit.h>
-#include <Adafruit_LittleFS.h>
-#include <InternalFileSystem.h>
 
-// BLE Service
-BLEDfu  bledfu;  // OTA DFU service
-BLEDis  bledis;  // device information
-BLEUart bleuart; // uart over ble
-BLEBas  blebas;  // battery
+#define DEVICE_NAME     "ClientNode0"
 
-void setup()
-{
-  Serial.begin(115200);
+BLEDfu  ble_dfu;            // OTA DFU service
+BLEDis  ble_device_info;   // device information
+BLEBas  ble_battery_info;    // battery
+BLEUart ble_uart;           // uart over ble
 
-#if CFG_DEBUG
-  // Blocking wait for connection when debug mode is enabled via IDE
-  while ( !Serial ) yield();
-#endif
-  
-  Serial.println("Bluefruit52 BLEUART Example");
-  Serial.println("---------------------------\n");
+BLEConnection* connection;
 
+void onConnectHandler(uint16_t conn_handle) {
+  // Get the reference to current connection
+  connection = Bluefruit.Connection(conn_handle);
+
+  char central_name[32] = {0};
+  connection->getPeerName(central_name, sizeof(central_name));
+}
+
+void onDisconnectHandler(uint16_t conn_handle, uint8_t reason) {
+}
+
+void BLE_init(void) {
   // Setup the BLE LED to be enabled on CONNECT
   // Note: This is actually the default behavior, but provided
   // here in case you want to control this LED manually via PIN 19
   Bluefruit.autoConnLed(true);
 
-  // Config the peripheral connection with maximum bandwidth 
+  // Config the peripheral connection with maximum bandwidth
   // more SRAM required by SoftDevice
   // Note: All config***() function must be called before begin()
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
 
   Bluefruit.begin();
   Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
-  //Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
-  Bluefruit.Periph.setConnectCallback(connect_callback);
-  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+  Bluefruit.setName(DEVICE_NAME); // useful testing with multiple central connections
 
-  // To be consistent OTA DFU should be added first if it exists
-  bledfu.begin();
+  Bluefruit.Periph.setConnectCallback(onConnectHandler);
+  Bluefruit.Periph.setDisconnectCallback(onDisconnectHandler);
 
-  // Configure and Start Device Information Service
-  bledis.setManufacturer("Adafruit Industries");
-  bledis.setModel("Bluefruit Feather52");
-  bledis.begin();
+  ble_dfu.begin();
 
-  // Configure and Start BLE Uart Service
-  bleuart.begin();
+  ble_device_info.setManufacturer("Rath Robotics");
+  ble_device_info.setModel("nRF52840");
+  ble_device_info.begin();
 
-  // Start BLE Battery Service
-  blebas.begin();
-  blebas.write(100);
-
-  // Set up and start advertising
-  startAdv();
-
-  Serial.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
-  Serial.println("Once connected, enter character(s) that you wish to send");
+  ble_battery_info.begin();
+  ble_battery_info.write(100);
+  
+  ble_uart.begin();
 }
 
-void startAdv(void)
-{
+void BLE_startAdvertising(void) {
   // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
 
   // Include bleuart 128-bit uuid
-  Bluefruit.Advertising.addService(bleuart);
+  Bluefruit.Advertising.addService(ble_uart);
 
   // Secondary Scan Response packet (optional)
   // Since there is no room for 'Name' in Advertising packet
   Bluefruit.ScanResponse.addName();
-  
+
   /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
+     - Enable auto advertising if disconnected
+     - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+     - Timeout for fast mode is 30 seconds
+     - Start(timeout) with timeout = 0 will advertise forever (until connected)
+
+     For recommended advertising interval
+     https://developer.apple.com/library/content/qa/qa1931/_index.html
+  */
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
 
-void loop()
-{
-  // Forward data from HW Serial to BLEUART
-  while (Serial.available())
-  {
-    // Delay to wait for enough input, since we have a limited transmission buffer
-    delay(2);
+void setup() {
+  Serial.begin(115200);
 
-    uint8_t buf[64];
-    int count = Serial.readBytes(buf, sizeof(buf));
-    bleuart.write( buf, count );
+  // while (!Serial) {}
+
+  Serial.setTimeout(100);
+  ble_uart.setTimeout(100);
+  
+  BLE_init();
+
+  BLE_startAdvertising();
+}
+
+void loop() {
+  // Forward data from HW Serial to BLEUART
+  if (Serial.available()) {
+    uint8_t buffer[128];
+    
+    uint16_t size = Serial.readBytesUntil('\n', buffer, 128);
+    ble_uart.write(buffer, size);
+    
+    // we DO NOT need to send another newline character here.
+    // `Serial.readBytesUntil()` will store the ending newline char into the buf.
   }
 
   // Forward from BLEUART to HW Serial
-  while ( bleuart.available() )
-  {
-    uint8_t ch;
-    ch = (uint8_t) bleuart.read();
-    Serial.write(ch);
+  if (ble_uart.available()) {
+    uint8_t buffer[128];
+    uint16_t size = ble_uart.readBytesUntil('\n', buffer, 128);
+    Serial.write(buffer, size);
+    
+    // we DO need to send another newline character here.
+    // `uart_svc.readBytesUntil()` will NOT store the ending newline char into the buf.
+    Serial.write('\n');
   }
-}
 
-// callback invoked when central connects
-void connect_callback(uint16_t conn_handle)
-{
-  // Get the reference to current connection
-  BLEConnection* connection = Bluefruit.Connection(conn_handle);
-
-  char central_name[32] = { 0 };
-  connection->getPeerName(central_name, sizeof(central_name));
-
-  Serial.print("Connected to ");
-  Serial.println(central_name);
-}
-
-/**
- * Callback invoked when a connection is dropped
- * @param conn_handle connection where this event happens
- * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
- */
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-  (void) conn_handle;
-  (void) reason;
-
-  Serial.println();
-  Serial.print("Disconnected, reason = 0x"); Serial.println(reason, HEX);
+  delay(1);
+  char test_str[] = "hello3___________________3\n";
+  ble_uart.write(test_str, 27);
 }
